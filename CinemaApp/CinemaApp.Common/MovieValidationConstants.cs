c0c29646c916bac3724403a555f90645778881a7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-23 18:32:12 +0300

    Added static class for Movie Validation Constants and Movie model configuration

---
+++ CinemaApp/CinemaApp.Common/MovieValidationConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Common
+{
+    public static class MovieValidationConstants
+    {
+        //Movie Lenght
+        public const int TitleMaxLenght = 50;
+        public const int TitleMinLenght = 2;
+
+        //Genre lenght
+        public const int GenreMaxLenght = 30;
+        public const int GenreMinLenght = 3;
+
+        //Director lenght
+        public const int DirectorMaxLenght = 50;
+        public const int DirectorMinLenght = 2;
+
+        //Description lenght
+        public const int DescriptionMaxLenght = 500;
+        public const int DescriptionMinLenght = 10;
+    }
+}
--- CinemaApp/CinemaApp.Data/Configurations/MovieConfiguration.cs
+++ CinemaApp/CinemaApp.Data/Configurations/MovieConfiguration.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CinemaApp.Common.MovieValidationConstants;
 
 namespace CinemaApp.Data.Configurations
 {
@@ -14,7 +15,35 @@ namespace CinemaApp.Data.Configurations
         public void Configure(EntityTypeBuilder<Movie> builder)
         {
             //Fluent API
-            throw new NotImplementedException();
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .Property(m => m.Title)
+                .IsRequired()
+                .HasMaxLength(TitleMaxLenght);
+
+            builder.Property(m => m.Genre)
+                .IsRequired()
+                .HasMaxLength(GenreMaxLenght);
+
+            builder
+                .Property(m => m.ReleaseDate)
+                .IsRequired();
+
+            builder
+                .Property(m => m.Director)
+                .IsRequired()
+                .HasMaxLength(DirectorMaxLenght);
+
+            builder
+                .Property(m => m.Duration)
+                .IsRequired();
+
+            builder
+                .Property(m => m.Description)
+                .IsRequired()
+                .HasMaxLength(DescriptionMaxLenght);
         }
     }
 }
