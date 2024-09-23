5cd4c68db52b09ae7586085c717984ff6610f044
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-23 20:30:20 +0300

    Added MinLength Validation in Movie Model and Update the DB

--- CinemaApp/CinemaApp.Data.Models/Movie.cs
+++ CinemaApp/CinemaApp.Data.Models/Movie.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CinemaApp.Common.MovieValidationConstants;
 
 namespace CinemaApp.Data.Models
 {
@@ -15,16 +17,21 @@ namespace CinemaApp.Data.Models
 
         public Guid Id { get; set; }
 
+        [MinLength(TitleMinLenght)]
         public string Title { get; set; } = null!;
 
+        [MinLength(GenreMinLenght)]
         public string Genre { get; set; } = null!;
 
         public DateTime ReleaseDate { get; set; }
 
+        [MinLength(DirectorMinLenght)]
         public string Director { get; set; } = null!;
 
+        [Range(MinDuration, MaxDuration)]
         public int Duration { get; set; }
 
+        [MinLength(DescriptionMinLenght)]
         public string Description { get; set; } = null!;
     }
 }
---
+++ CinemaApp/CinemaApp.Data/Migrations/20240923172627_AddedMoviePropertiesMinLenghtValidation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CinemaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMoviePropertiesMinLenghtValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("7be0b043-5a67-4a21-a594-9edae1eccc61"));
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("bd390e17-b0c2-4426-93a6-6d0cee1088e5"));
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Director", "Duration", "Genre", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("2e4cab9b-56a4-4d52-8058-6cc9f503c32d"), "After the Empire overpowers the Rebel Alliance, Luke Skywalker begins his Jedi training with Yoda. At the same time, Darth Vader and bounty hunter Boba Fett pursue his friends across the galaxy.", "Irvin Kershner", 124, "Action Epic", new DateTime(1980, 5, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Star Wars: Episode V - The Empire Strikes Back" },
+                    { new Guid("3f659b18-6721-415e-878b-ddd3279167d5"), "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", "Francis Ford Coppola", 175, "Gangster", new DateTime(1972, 3, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Godfather" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("2e4cab9b-56a4-4d52-8058-6cc9f503c32d"));
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("3f659b18-6721-415e-878b-ddd3279167d5"));
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Director", "Duration", "Genre", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("7be0b043-5a67-4a21-a594-9edae1eccc61"), "After the Empire overpowers the Rebel Alliance, Luke Skywalker begins his Jedi training with Yoda. At the same time, Darth Vader and bounty hunter Boba Fett pursue his friends across the galaxy.", "Irvin Kershner", 124, "Action Epic", new DateTime(1980, 5, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "Star Wars: Episode V - The Empire Strikes Back" },
+                    { new Guid("bd390e17-b0c2-4426-93a6-6d0cee1088e5"), "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", "Francis Ford Coppola", 175, "Gangster", new DateTime(1972, 3, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Godfather" }
+                });
+        }
+    }
+}
