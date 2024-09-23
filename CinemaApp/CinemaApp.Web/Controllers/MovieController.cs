33083ab4d9fbdb6f4e509fdd3a75e9c7a5effae5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-23 19:26:17 +0300

    Added Movie Details View

--- CinemaApp/CinemaApp.Web/Controllers/MovieController.cs
+++ CinemaApp/CinemaApp.Web/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using CinemaApp.Data;
+﻿using Azure.Core;
+using CinemaApp.Data;
 using CinemaApp.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,26 @@ namespace CinemaApp.Web.Controllers
             IEnumerable<Movie> allMovies = await _context.Movies.ToListAsync();
 
             return View(allMovies);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string movieId)
+        {
+            bool isIdGuid = Guid.TryParse(movieId, out Guid idGuid);
+
+            if (!isIdGuid)
+            {
+                return View(nameof(Index));
+            }
+
+            Movie? movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == idGuid);
+
+            if(movie is null)
+            {
+                return NotFound();
+            }
+
+           return View(movie);
         }
     }
 }
