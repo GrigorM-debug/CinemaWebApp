62b00898b3c2115a367696e1841e90c15ed49731
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-23 18:13:43 +0300

    Added Movie Model

--- CinemaApp/CinemaApp.Data.Models/Movie.cs
+++ CinemaApp/CinemaApp.Data.Models/Movie.cs
@@ -8,5 +8,23 @@ namespace CinemaApp.Data.Models
 {
     public class Movie
     {
+        public Movie()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Genre { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Director { get; set; } = null!;
+
+        public int Duration { get; set; }
+
+        public string Description { get; set; } = null!;
     }
 }
