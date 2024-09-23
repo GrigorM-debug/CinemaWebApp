94403f9e50e76e40111187037db0165f9d0712b7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-23 19:10:59 +0300

    Added Movie Controller and Index movie View. Added nav bar li for Movies page

---
+++ CinemaApp/CinemaApp.Web/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaApp.Data;
+using CinemaApp.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly CinemaAppDbContext _context;
+
+        public MovieController(CinemaAppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Movie> allMovies = await _context.Movies.ToListAsync();
+
+            return View(allMovies);
+        }
+    }
+}
