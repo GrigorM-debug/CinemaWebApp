84cbc1f8cf3f349ffaf668aadea79be6f93b0ecd
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-09-30 19:41:38 +0300

    Added Cinema Model

---
+++ CinemaApp/CinemaApp.Data.Models/Cinema.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Data.Models
+{
+    public class Cinema
+    {
+        public Cinema()
+        {
+            Id = Guid.NewGuid();
+            CinemasMovies = new HashSet<CinemaMovie>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Location { get; set; } = null!;
+
+        public virtual ICollection<CinemaMovie> CinemasMovies { get; set; }
+    }
+}
