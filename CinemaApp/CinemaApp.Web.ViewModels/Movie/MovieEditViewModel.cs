f286c1ae8a8b7cc78fe389592ca891e75cd2b010
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-01 23:50:33 +0300

    Added CinemaEditViewModel

--- CinemaApp/CinemaApp.Web.ViewModels/Movie/MovieEditViewModel.cs
+++ CinemaApp/CinemaApp.Web.ViewModels/Movie/MovieEditViewModel.cs
@@ -4,35 +4,23 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static CinemaApp.Common.MovieValidationConstants;
-using static CinemaApp.Common.EntityValidations.MovieValidations;
 
 namespace CinemaApp.Web.ViewModels.Movie
 {
     public class MovieEditViewModel
     {
 
-        [Required(ErrorMessage = TitleIsRequiredMessage)]
-        [MinLength(TitleMinLenght)]
-        [MaxLength(TitleMaxLenght)]
         public string Title { get; set; } = null!;
 
-        [Required(ErrorMessage = GenreIsRequiredMessage)]
-        [MinLength(GenreMinLenght)]
-        [MaxLength(GenreMaxLenght)]
+    
         public string Genre { get; set; } = null!;
 
         public DateTime ReleaseDate { get; set; }
 
-        [Required(ErrorMessage = DirectorNameIsRequiredMessage)]
-        [MinLength(DirectorMinLenght)]
-        [MaxLength(DirectorMaxLenght)]
         public string Director { get; set; } = null!;
 
-        [Range(MinDuration, MaxDuration)]
         public int Duration { get; set; }
 
-        [MinLength(DescriptionMinLenght)]
         public string Description { get; set; } = null!;
     }
 }
