5c983d3c9bbbebcfdb75771a1a5f3ebf158d7d47
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-01 10:14:46 +0300

    Added XML Summary comments in CinemaMovieConfiguration

--- CinemaApp/CinemaApp.Data/Configurations/CinemaMovieConfiguration.cs
+++ CinemaApp/CinemaApp.Data/Configurations/CinemaMovieConfiguration.cs
@@ -1,31 +1,38 @@
 ﻿using CinemaApp.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CinemaApp.Data.Configurations
 {
+    /// <summary>
+    /// Configures the <see cref="CinemaMovie"/> entity for the database model.
+    /// This class defines the primary key and relationships for the <see cref="CinemaMovie"/> entity.
+    /// </summary>
     public class CinemaMovieConfiguration : IEntityTypeConfiguration<CinemaMovie>
     {
+        /// <summary>
+        /// Configures the properties and relationships of the <see cref="CinemaMovie"/> entity.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="EntityTypeBuilder{TEntity}"/> used to configure the <see cref="CinemaMovie"/> entity.
+        /// </param>
         public void Configure(EntityTypeBuilder<CinemaMovie> builder)
         {
+            // Set the primary key for the CinemaMovie entity, which is a composite key consisting of MovieId and CinemaId
             builder
-                .HasKey(cm => new { cm.MovieId, cm.CinemaId});
+                .HasKey(cm => new { cm.MovieId, cm.CinemaId });
 
-
+            // Configure the relationship between CinemaMovie and Movie
             builder
-                .HasOne(cm => cm.Movie)
-                .WithMany(m => m.CinemasMovies)
-                .HasForeignKey(cm => cm.MovieId);
+                .HasOne(cm => cm.Movie)                    
+                .WithMany(m => m.CinemasMovies)            
+                .HasForeignKey(cm => cm.MovieId);           
 
+            // Configure the relationship between CinemaMovie and Cinema
             builder
-                .HasOne(cm => cm.Cinema)
-                .WithMany(c => c.CinemasMovies)
-                .HasForeignKey(cm => cm.CinemaId);
+                .HasOne(cm => cm.Cinema)                    
+                .WithMany(c => c.CinemasMovies)             
+                .HasForeignKey(cm => cm.CinemaId);          
         }
     }
 }
