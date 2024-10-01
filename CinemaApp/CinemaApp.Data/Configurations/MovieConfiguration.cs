20a3d54555f32acd40de6acb299a83049c159647
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-01 10:12:59 +0300

    Added XML Summary comments in CinemaConfiguration

--- CinemaApp/CinemaApp.Data/Configurations/MovieConfiguration.cs
+++ CinemaApp/CinemaApp.Data/Configurations/MovieConfiguration.cs
@@ -1,80 +1,67 @@
-﻿using Azure.Core;
-using CinemaApp.Data.Models;
+﻿using CinemaApp.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static CinemaApp.Common.MovieValidationConstants;
 
 namespace CinemaApp.Data.Configurations
 {
+    /// <summary>
+    /// Configures the <see cref="Movie"/> entity for the database model.
+    /// This class defines the primary key, required properties, and maximum length for the <see cref="Movie"/> entity.
+    /// </summary>
     public class MovieConfiguration : IEntityTypeConfiguration<Movie>
     {
+        /// <summary>
+        /// Configures the properties and relationships of the <see cref="Movie"/> entity.
+        /// </summary>
+        /// <param name="builder">
+        /// The <see cref="EntityTypeBuilder{TEntity}"/> used to configure the <see cref="Movie"/> entity.
+        /// </param>
         public void Configure(EntityTypeBuilder<Movie> builder)
         {
-            //Fluent API
+            // Set the primary key for the Movie entity
             builder
                 .HasKey(m => m.Id);
 
+            // Configure the Title property
             builder
-                .Property(m => m.Title)
-                .IsRequired()
-                .HasMaxLength(TitleMaxLenght);
+                .Property(m => m.Title)                    
+                .IsRequired()                               
+                .HasMaxLength(TitleMaxLenght)             
+                .HasComment("The title of the movie. Must be at least the minimum length specified."); 
 
-            builder.Property(m => m.Genre)
-                .IsRequired()
-                .HasMaxLength(GenreMaxLenght);
+            // Configure the Genre property
+            builder
+                .Property(m => m.Genre)
+                .IsRequired()                               
+                .HasMaxLength(GenreMaxLenght)              
+                .HasComment("The genre of the movie. Must be at least the minimum length specified."); 
 
+            // Configure the ReleaseDate property
             builder
                 .Property(m => m.ReleaseDate)
-                .IsRequired();
+                .IsRequired()                               
+                .HasComment("The release date of the movie."); 
 
+            // Configure the Director property
             builder
-                .Property(m => m.Director)
-                .IsRequired()
-                .HasMaxLength(DirectorMaxLenght);
+                .Property(m => m.Director)                 
+                .IsRequired()                              
+                .HasMaxLength(DirectorMaxLenght)           
+                .HasComment("The director of the movie. Must be at least the minimum length specified."); 
 
+            // Configure the Duration property
             builder
                 .Property(m => m.Duration)
-                .IsRequired();
+                .IsRequired()                               
+                .HasComment("The duration of the movie in minutes."); 
 
+            // Configure the Description property
             builder
-                .Property(m => m.Description)
-                .IsRequired()
-                .HasMaxLength(DescriptionMaxLenght);
-
-            //builder.HasData(MovieDataSeed());
+                .Property(m => m.Description)              
+                .IsRequired()                               
+                .HasMaxLength(DescriptionMaxLenght)      
+                .HasComment("A brief description of the movie. Must be at least the minimum length specified."); 
         }
-
-        //private List<Movie> MovieDataSeed()
-        //{
-        //    List<Movie> movies = new List<Movie>()
-        //    {
-        //        new Movie()
-        //        {
-        //            Title = "Star Wars: Episode V - The Empire Strikes Back",
-        //            Genre = "Action Epic",
-        //            ReleaseDate = new DateTime(1980, 05, 06),
-        //            Director = "Irvin Kershner",
-        //            Duration = 124,
-        //            Description = "After the Empire overpowers the Rebel Alliance, Luke Skywalker begins his Jedi training with Yoda. At the same time, Darth Vader and bounty hunter Boba Fett pursue his friends across the galaxy."
-        //        },
-
-        //        new Movie()
-        //        {
-        //            Title = "The Godfather",
-        //            Genre = "Gangster",
-        //            ReleaseDate = new DateTime(1972, 03, 14),
-        //            Director = "Francis Ford Coppola",
-        //            Duration = 175,
-        //            Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
-        //        }
-        //    };
-
-        //    return movies;
-        //}
     }
 }
