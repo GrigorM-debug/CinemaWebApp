18066a236f55ff1d2af3713e99cd1b34e0c25ba6
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-01 23:59:18 +0300

    Added CinemaEditInputViewModel

---
+++ CinemaApp/CinemaApp.Web.ViewModels/Cinema/CinemaEditInputViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CinemaApp.Common.CinemaValidationConstants;
+using static CinemaApp.Common.EntityValidations.CinemaValidations;
+
+namespace CinemaApp.Web.ViewModels.Cinema
+{
+    public class CinemaEditInputViewModel
+    {
+        [Required(ErrorMessage = NameIsRequredMessage)]
+        [MinLength(NameMinLenght, ErrorMessage = NameLenghtMessage)]
+        [MaxLength(NameMaxLenght, ErrorMessage = NameLenghtMessage)]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = NameIsRequredMessage)]
+        [MinLength(LocationMinLengh, ErrorMessage = LocationLenghtMessage)]
+        [MaxLength(LocationMaxLenght, ErrorMessage = LocationLenghtMessage)]
+        public string Location { get; set; } = null!;
+    }
+}
